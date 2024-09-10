39e1b196c7a3b276795b716ffa4b3b204faa303a
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-10 13:24:34 -0300

    Exercicios até o 5 da aula 09

--- ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio5.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio5.java
@@ -4,6 +4,15 @@ public class Exercicio5 {
 
 	public static void main(String[] args) {
 
-	}
+		 String[] meses = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", 
+                 "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
 
+
+		 int[] dias = {31, 28, 31, 30, 30, 30, 30, 30, 30, 30, 30, 31};
+
+
+		 for (int i = 0; i < meses.length; i++) {
+			 System.out.println("O mês de " + meses[i] + " tem " + dias[i] + " dias ao todo.");
+		 		}
+	}
 }
