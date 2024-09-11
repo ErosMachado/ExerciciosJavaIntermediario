ad97463722971484b239036e8c229e0c7d01a9f5
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-11 13:18:54 -0300

    exercicio 7 da aula 09.

--- ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio7.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio7.java
@@ -1,15 +1,31 @@
 package ExerciciosAula09;
+
 import java.util.Scanner;
 
 public class Exercicio7 {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Digite um nome: ");
-		//String nome = scan.nextLine();
-		
-		
-		
-		scan.close();
-	}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] nomes = new String[100]; 
+        int count = 0;
+        
+        while (true) {
+            System.out.print("Digite um nome (ou pressione Enter para finalizar): ");
+            String nome = scan.nextLine();
+            
+            
+            if (nome.length() == 0) {
+                break; 
+            }
+            
+            nomes[count] = nome; 
+            count++; 
+        }
+        
+        System.out.println("\nNomes na ordem inversa:");
+        for (int i = count - 1; i >= 0; i--) {
+            System.out.println(nomes[i]); 
+        }
+        
+        scan.close();
+    }
 }
--- ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio8.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio8.java
@@ -1,10 +1,7 @@
 package ExerciciosAula09;
 
 public class Exercicio8 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
