9b81fe8e4e95dc8dcbb46b72e9da6396514b55f9
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-16 16:31:58 -0300

    Exercicio 9 da aula 09 resolvido.

--- ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio9.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio9.java
@@ -1,10 +1,23 @@
 package ExerciciosAula09;
-
 public class Exercicio9 {
 
 	public static void main(String[] args) {
+		int [][] matrizMinhaQueridaMatriz = new int[3][4];
+		int num = 0;
 		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				matrizMinhaQueridaMatriz[i][j] = num;
+				num++;
+			}
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(matrizMinhaQueridaMatriz[i][j] + "\t");
+			}
+		System.out.println(""); 
+			}
 
-	}
-
-}
+	}//fim
+}//fim
--- ExerciciosJavaIntermediario/src/ExerciciosAula09/QExercicio10.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/QExercicio10.java
@@ -3,8 +3,6 @@ package ExerciciosAula09;
 public class QExercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
