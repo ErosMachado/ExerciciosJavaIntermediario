0b61f8c71686172386d6dccec9d4a6f0a80393eb
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-16 16:06:33 -0300

    exercicio 8 da aula 09 resolvido

--- ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio8.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio8.java
@@ -1,23 +1,33 @@
-package ExerciciosAula09; //ainda não completo 11/09
+package ExerciciosAula09;
 
 import java.util.Scanner;
 
 public class Exercicio8 {
-	public static void main(String[] args) {
-		int i =0;
-		
-		try (Scanner scan = new Scanner(System.in)) {
-			System.out.println("Digite um número inteiro positivo: ");
-			int n = scan.nextInt();
-			char[] caractereNovo =  new char[n];
-			
-			
-			for(i=0; i<n; i++) {
-				System.out.println("Digite um caractere qualquer: ");
-				caractereNovo[i]  = scan.nextLine().charAt(i); //essa linha está dando erro
-			}
-			
-			scan.close();
-		}
-	}
+    public static void main(String[] args) {
+        int i;
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Digite um número inteiro positivo: ");
+            int n = scan.nextInt();
+            char[] caractereNovo = new char[n];
+
+            for (i = 0; i < n; i++) {
+                System.out.println("Digite um caractere qualquer: ");
+                caractereNovo[i] = scan.next().charAt(0); 
+            }
+
+         
+            for (i = 0; i < n / 2; i++) {
+                char temp = caractereNovo[i];
+                caractereNovo[i] = caractereNovo[n - 1 - i];
+                caractereNovo[n - 1 - i] = temp;
+            }
+
+            System.out.println("Vetor invertido:");
+            for (char c : caractereNovo) {
+                System.out.print(c + " ");
+            }
+            System.out.println(); 
+        }
+    }
 }
--- ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio9.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula09/Exercicio9.java
@@ -3,7 +3,7 @@ package ExerciciosAula09;
 public class Exercicio9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
