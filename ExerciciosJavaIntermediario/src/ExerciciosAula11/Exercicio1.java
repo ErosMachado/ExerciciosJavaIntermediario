4fd0ad207374c919a4ccc08d3cebdf46a1bb195d
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-19 13:36:47 -0300

    Exercicios 1 e 2 da aula 11 feitos.

--- ExerciciosJavaIntermediario/src/ExerciciosAula11/Exercicio1.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula11/Exercicio1.java
@@ -1,10 +1,25 @@
 package ExerciciosAula11;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        System.out.print("Digite a largura do terreno: ");
+        double largura = scanner.nextDouble();
 
+        System.out.print("Digite o comprimento do terreno: ");
+        double comprimento = scanner.nextDouble();
+
+        double area = calcularArea(largura, comprimento);
+
+        System.out.println("A área do terreno é: " + area + " metros quadrados");
+
+        scanner.close();
+    }
+    public static double calcularArea(double largura, double comprimento) {
+        return largura * comprimento;
+    }
 }
--- ExerciciosJavaIntermediario/src/ExerciciosAula11/Exercicio2.java
+++ ExerciciosJavaIntermediario/src/ExerciciosAula11/Exercicio2.java
@@ -1,10 +1,29 @@
 package ExerciciosAula11;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        System.out.print("Digite a idade da pessoa: ");
+        int idade = scanner.nextInt();
 
+        String resultado = verificarVoto(idade);
+
+        System.out.println("Para essa idade, o voto é: " + resultado);
+
+        scanner.close();
+    }
+
+    public static String verificarVoto(int idade) {
+        if (idade < 16) {
+            return "Proibido";
+        } else if (idade >= 18 && idade <= 70) {
+            return "Obrigatório";
+        } else {
+            return "Opcional";
+        }
+    }
 }
